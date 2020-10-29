7cd6d6e8bb3ad6d88ee640bbf80fdbde1a45cae3
Roman <adam.romanowski@ready4s.com>
2020-10-29 14:46:42 +0100

    Initial repo from previous workshop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rxjsapp';
+export class AppComponent implements OnInit {
+  title = 'Title';
+  names$: Observable<string[]> = of(['John', 'Bob', 'Nick']);
+  searchTerm: string;
+  results: Observable<string[]>;
+
+  constructor(private http: HttpClient) {
+  }
+
+  newSearch(searchTerm: string) {
+    this.results = this.http.get(`https://api.github.com/search/repositories?q=${searchTerm}&sort=stars&order=desc`)
+      .pipe(
+        map(results => results.items)
+      );
+  }
+
+  ngOnInit(): void {
+  }
 }
