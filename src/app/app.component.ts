bcc9aa9c864bb9ef0d86a323d511980af881d94f
Roman <adam.romanowski@ready4s.com>
2020-10-30 15:56:54 +0100

    Add operators to adjust input behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,44 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, of, Subject} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {map, tap} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, filter, map, switchMap, takeUntil, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Title';
   names$: Observable<string[]> = of(['John', 'Bob', 'Nick']);
   searchTerm: string;
   results: Observable<string[]>;
+  searchTerm$: Subject<string> = new Subject<string>();
+  stop$: Subject<string> = new Subject<string>();
 
   constructor(private http: HttpClient) {
   }
 
-  newSearch(searchTerm: string) {
-    this.results = this.http.get(`https://api.github.com/search/repositories?q=${searchTerm}&sort=stars&order=desc`)
+  newSearch(searchTerm: string): void {
+    this.searchTerm$.next(searchTerm);
+  }
+
+  ngOnInit(): void {
+    this.results = this.searchTerm$.asObservable()
       .pipe(
+        takeUntil(this.stop$),
+        debounceTime(500),
+        filter((term: string) => !!term),
+        distinctUntilChanged(),
+        switchMap((term: string) => this.http
+          .get(`https://api.github.com/search/repositories?q=${term}&sort=stars&order=desc`)
+        ),
         map(results => results.items)
       );
   }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.stop$.next();
+    this.stop$.complete();
   }
 }
